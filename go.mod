b03451d0224b38d74fce28a64f7b11f646499807
unknown <yistabraq@gmail.com>
2022-04-04 15:50:40 +0000

    add class transaction

--- go.mod
+++ go.mod
@@ -3,13 +3,19 @@ module reporting
 go 1.18
 
 require (
+	github.com/tobgu/qframe v0.3.6
+	github.com/xuri/excelize/v2 v2.5.0
+)
+
+require (
+	github.com/golang/snappy v0.0.4 // indirect
+	github.com/klauspost/compress v1.15.1 // indirect
 	github.com/mauricelam/genny v0.0.0-20190320071652-0800202903e5 // indirect
 	github.com/mohae/deepcopy v0.0.0-20170929034955-c48cc78d4826 // indirect
+	github.com/pierrec/lz4/v4 v4.1.14 // indirect
 	github.com/richardlehane/mscfb v1.0.3 // indirect
 	github.com/richardlehane/msoleps v1.0.1 // indirect
-	github.com/tobgu/qframe v0.3.6 // indirect
 	github.com/xuri/efp v0.0.0-20210322160811-ab561f5b45e3 // indirect
-	github.com/xuri/excelize/v2 v2.5.0 // indirect
 	golang.org/x/crypto v0.0.0-20210711020723-a769d52b0f97 // indirect
 	golang.org/x/net v0.0.0-20210726213435-c6fcb2dbf985 // indirect
 	golang.org/x/text v0.3.6 // indirect
--- main.go
+++ main.go
@@ -1,32 +1,95 @@
 package main
 
 import (
+	"encoding/csv"
 	"fmt"
 	"os"
+	"strconv"
 	"time"
 
 	"github.com/tobgu/qframe"
-	"github.com/tobgu/qframe/config/csv"
+	fcsv "github.com/tobgu/qframe/config/csv"
 	"github.com/tobgu/qframe/types"
 	"github.com/xuri/excelize/v2"
 )
 
 func main() {
-	file, err := os.Open("vue.csv")
+	file, err := os.Open("acq_trans.csv")
+	if err != nil {
+		check(err)
+	}
+	defer file.Close()
+	t0 := time.Now()
+	qf := qframe.ReadCSV(file, fcsv.Delimiter(byte('|')))
+	fmt.Println("Read data duration : ", time.Since(t0))
+	t0 = time.Now()
+	qf = qf.Apply(qframe.Instruction{Fn: update, SrcCol1: "REVERSAL", SrcCol2: "ACQ_AMOUNT", DstCol: "ACQ_AMOUNT"})
+	qf = qf.Apply(qframe.Instruction{Fn: update, SrcCol1: "REVERSAL", SrcCol2: "ISS_AMOUNT", DstCol: "ISS_AMOUNT"})
+	fmt.Println("Update reversal duration : ", time.Since(t0))
+	for _, v := range []int{1001, 1002, 1003, 1004, 1005, 1006, 1007, 1008, 1010, 1011, 1012, 1013} {
+		t0 = time.Now()
+		To_Excel(qf, v)
+		fmt.Printf("Duration write template %d: %v\n", v, time.Since(t0))
+	}
+}
+func check(err error) {
 	if err != nil {
 		fmt.Println(err)
-		return
+		os.Exit(1)
 	}
-	delimiter := byte('|')
-	t0 := time.Now()
-	out := qframe.ReadCSV(file, csv.Delimiter(delimiter))
-	fmt.Println(time.Since(t0))
-	qf := out.Filter(qframe.Filter{Column: "ISS_INST", Arg: 1001, Comparator: "="})
-	f := excelize.NewFile()
-	named := qf.ColumnTypeMap()
+}
+
+func get_response() map[int]string {
+	f, err := os.Open("response.csv")
+	if err != nil {
+		check(err)
+	}
+
+	// remember to close the file at the end of the program
+	defer f.Close()
+	csvReader := csv.NewReader(f)
+	data, err := csvReader.ReadAll()
+	if err != nil {
+		check(err)
+	}
+	output := make(map[int]string)
+	for _, v := range data {
+		key, err := strconv.Atoi(v[0])
+		if err != nil {
+			continue
+		}
+		output[key] = v[1]
+	}
+	return output
+}
+
+func Get_desc(input int, dict map[int]string) string {
+	if val, ok := dict[input]; ok {
+		return val
+	}
+	return "Approuved"
+}
+func get_action(value int) string {
+	if value == -1 {
+		return "Approuved"
+	}
+	return "Rejected"
+}
+
+func To_Excel(qf qframe.QFrame, inst int) {
+	qf = qf.Filter(qframe.Filter{Column: "ACQ_INST", Arg: inst, Comparator: "="})
+	if qf.Err != nil {
+		check(qf.Err)
+	}
+	outfile := fmt.Sprintf("template/template_%d.xlsx", inst)
+	f, err := excelize.OpenFile(outfile, excelize.Options{})
+	check(err)
+	f.DeleteSheet("Dataset")
 	f.NewSheet("Dataset")
+	named := qf.ColumnTypeMap()
 	c := 'A'
 	last := ""
+	response := get_response()
 	for _, col := range qf.ColumnNames() {
 		err := f.SetCellValue("Dataset", fmt.Sprintf("%c1", c), col)
 		if err != nil {
@@ -41,7 +104,6 @@ func main() {
 				if err != nil {
 					panic(err)
 				}
-				last = fmt.Sprintf("%c%d", c, i+2)
 			}
 		case types.Int:
 			view := qf.MustIntView(col)
@@ -51,13 +113,40 @@ func main() {
 				if err != nil {
 					panic(err)
 				}
-				last = fmt.Sprintf("%c%d", c, i+2)
+			}
+			if col == "RESP" {
+				c++
+				err := f.SetCellValue("Dataset", fmt.Sprintf("%c1", c), "ACTION")
+				if err != nil {
+					panic(err)
+				}
+				for i := 0; i < view.Len(); i++ {
+					value := view.ItemAt(i)
+					err := f.SetCellValue("Dataset", fmt.Sprintf("%c%d", c, i+2), get_action(value))
+					if err != nil {
+						panic(err)
+					}
+				}
+				c++
+				err = f.SetCellValue("Dataset", fmt.Sprintf("%c1", c), "REJECTED_TYPE")
+				if err != nil {
+					panic(err)
+				}
+				for i := 0; i < view.Len(); i++ {
+					value := view.ItemAt(i)
+					err := f.SetCellValue("Dataset", fmt.Sprintf("%c%d", c, i+2), Get_desc(value, response))
+					if err != nil {
+						panic(err)
+					}
+				}
 			}
 		}
 		c++
 	}
+	last = fmt.Sprintf("%c%d", c-1, qf.Len()+1)
+	fmt.Println(last)
 	err = f.AddTable("Dataset", "A1", last, `{
-		"table_name": "table",
+		"table_name": "acq_trans",
 		"table_style": "TableStyleLight14",
 		"show_first_column": true,
 		"show_last_column": true,
@@ -65,10 +154,16 @@ func main() {
 		"show_column_stripes": true
 	}`)
 	if err != nil {
-		fmt.Println(err)
-		return
+		check(err)
 	}
-	if err := f.SaveAs("Book1.xlsx"); err != nil {
+	if err := f.SaveAs(outfile); err != nil {
 		fmt.Println(err)
 	}
+	os.Exit(0)
+}
+func update(a, b int) int {
+	if a == 1 {
+		return -b
+	}
+	return b
 }
